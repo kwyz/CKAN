9b4368e5747ba4bc2c39de2daa64563fd1925869
Vasile Mustuc <mustuc.vasile@gmail.com>
2021-06-24 09:21:03 +0300

    adjust appearance for faq page

--- ckanext/dategovmd_theme/plugin.py
+++ ckanext/dategovmd_theme/plugin.py
@@ -3,7 +3,7 @@ import ckan.plugins.toolkit as tk
 import requests
 from ckan.lib.plugins import DefaultTranslation
 from ckanext.dategovmd_theme.blueprint import dategovmd as dategovmd_blueprint
-from jinja2 import Template
+
 
 def faq_items():
     response = requests.get(tk.config.get('infoportal.faq_url'))
@@ -11,10 +11,6 @@ def faq_items():
     faq_items = response_data['items']
     return faq_items
 
-def render_as_template(raw_html):
-    raw_template = Template(raw_html)
-    rendered_template = raw_template.render()
-    return rendered_template
 
 class Dategovmd_ThemePlugin(p.SingletonPlugin, DefaultTranslation):
     p.implements(p.ITranslation)
@@ -34,5 +30,4 @@ class Dategovmd_ThemePlugin(p.SingletonPlugin, DefaultTranslation):
 
     # ITemplateHelpers
     def get_helpers(self):
-        return {'faq_list': faq_items,
-		'render_as_template': render_as_template}
+        return {'faq_list': faq_items}
